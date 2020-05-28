43b62171940ae8ecc1f25377915a7109b19ff074
unknown <alfianizzah6@gmail.com>
2020-05-28 22:05:18 +0800

    Mengubah file template all

--- README.md
+++
@@ -1,4 +0,0 @@
-# alfianizzah.github.io
-Web template
-ini adalah web template yang bisa didownload siapa tau menarik
-save save
---
+++ script.js
@@ -0,0 +1,16 @@
+const sidenav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sidenav);
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+    indicators: true,
+    duration: 500,
+    interval: 4000
+});
+const collapsible = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsible);
+const scroolSpy = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroolSpy, {
+    scrollOffset: 90
+});
+const materialboxed = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialboxed);
